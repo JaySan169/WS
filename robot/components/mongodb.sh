60b36279d707bd33f4a93eeee80f539e02e9c930
JaySan169 <sanjay01061990@gmail.com>
2023-03-30 16:54:22 +0530

    1st

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -18,7 +18,7 @@ stat $?
 
 
 echo -n "updating mongodb config file :"
-sed -e -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' mongod.conf
 stat $?
 
 echo -n "starting mongodb :"
