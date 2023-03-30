a3c7b11963077a1d2d9789d0f4f20c32e3f6e95e
JaySan169 <sanjay01061990@gmail.com>
2023-03-30 17:50:26 +0530

    1st

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -10,7 +10,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> $LOGFILE
 stat $?
 
 echo -n "Installing Node JS :"
-yum install nodejs make gcc-c++ -y &>> $LOGFILE
+yum install nodejs -y &>> $LOGFILE
 stat $?
 
 id $APPUSER &>> $LOGFILE
