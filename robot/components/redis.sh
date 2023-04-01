db20565a789d25b20ac430dd78ee2aafbc2d5110
JaySan169 <sanjay01061990@gmail.com>
2023-04-01 15:04:49 +0530

    1st

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -7,21 +7,15 @@ echo -n "configuring $COMPONENT repo:"
 wget https://download.redis.io/releases/redis-6.2.7.tar.gz &>> $LOGFILE
 tar -xvf redis-6.2.7.tar.gz &>> $LOGFILE
 cd ./redis-6.2.7/
-#yum install epel-release yum-utils -y &>> $LOGFILE
-#yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>> $LOGFILE
-#yum-config-manager --enable remi &>> $LOGFILE
-#curl -L "https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/redis.repo" -o /etc/yum.repos.d/redis.repo &>> $LOGFILE
 stat $?
 
 
 echo -n "Installing $COMPONENT:"
-#yum install redis -y &>> $LOGFILE
 yum install redis-* -y &>> $LOGFILE
 stat $?
 
 echo -n "whitelisting redis to others :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>> $LOGFILE
-#sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>> $LOGFILE
 stat $?
 
 echo -n "Starting redis service:"
