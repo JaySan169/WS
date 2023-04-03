5626a8f87a634783213f2b83f4b260665ac3ad07
JaySan169 <sanjay01061990@gmail.com>
2023-04-03 16:40:32 +0530

    1st

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -9,6 +9,7 @@ curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stan
 stat $?
 
 echo -n "Installing $COMPONENT:"
+yum remove mariadb-libs -y &>> $LOGFILE
 yum install mysql-community-server -y &>> $LOGFILE
 stat $?
 
