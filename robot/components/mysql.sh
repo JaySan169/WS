4aa7ce5c63825712b6f95d74b7f218a8abfdf224
JaySan169 <sanjay01061990@gmail.com>
2023-04-03 14:50:16 +0530

    1st

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -e
+
 COMPONENT=mysql
 source components/common.sh
+
+read -p 'Enter MySQl password you wish to configure:' MYSQL_PWD
 
 echo -n "configuring the $COMPONENT repo:" 
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE
 stat $?
 
 echo -n "Installing $COMPONENT:"
-yum remove mariadb-libs -y &>> $LOGFILE
 yum install mysql-community-server -y &>> $LOGFILE
 stat $?
 
@@ -20,17 +21,17 @@ stat $?
 echo -n "Changing the default pasword:"
 DEF_ROOT_PASSWORD=$(grep 'A temporary password' var/log/mysqld.log | awk -F ' ' '{print $NF}') &>> $LOGFILE
 
-echo show databases | mysql -uroot -pRoboShop@1
+echo show databases | mysql -uroot -p${MYSQL_PWD}
 if [ $? -ne 0 ] ; then
 echo -n "Reset root password:"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PWD}';" | mysql --connect-expired-password -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE
 stat $?
 fi
 
-echo show plugins | mysql -uroot -pRoboShop@1 | grep validate_password; 
+echo show plugins | mysql -uroot -p${MYSQL_PWD} | grep validate_password; 
 if [ $? -eq 0 ] ; then
 echo -n "uninstalling password validate plugin"
-echo "uninstall plugin validate_password;" | mysql --connect-expired-password -uroot -pRoboShop@1
+echo "uninstall plugin validate_password;" | mysql --connect-expired-password -uroot -p${MYSQL_PWD}
 stat $?
 fi
 
@@ -41,7 +42,7 @@ unzip -o $COMPONENT.zip &>> $LOGFILE
 
 echo -n "Injecting the $COMPONENT Schema: "
 cd /tmp/$COMPONENT-main
-mysql -u root -pRoboShop@1 <shipping.sql &>> $LOGFILE
+mysql -u root -p${MYSQL_PWD} <shipping.sql &>> $LOGFILE
 
 
 echo -n -e "\e[32m___________ $COMPONENT installation completed______________\e[0m"
