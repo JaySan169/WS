ea103a036019956c92ac9d147609878d6daedbc5
JaySan169 <sanjay01061990@gmail.com>
2023-04-07 15:32:52 +0530

    1st

--- robot/components/rabbitmq.sh
+++ robot/components/rabbitmq.sh
@@ -4,18 +4,18 @@ source components/common.sh
 
 
 echo -n "Configuring $COMPONENT repo:"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
+yum install https://github.com/$COMPONENT/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
+curl -s https://packagecloud.io/install/repositories/$COMPONENT/$COMPONENT-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $?
 
 
 echo -n "installing $COMPONENT:"
-yum install rabbitmq-server -y &>> $LOGFILE
+yum install $COMPONENT-server -y &>> $LOGFILE
 stat $?
 
 echo -n "Starting $COMPONENT:"
-systemctl enable rabbitmq-server &>> $LOGFILE
-systemctl start rabbitmq-server
+systemctl enable $COMPONENT-server &>> $LOGFILE
+systemctl start $COMPONENT-server
 stat $?
 
 echo -n "Configuring User and permissons:"
