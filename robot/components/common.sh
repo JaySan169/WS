9abfc5dc51de641dd5e3b5b13fc5a3a0cf2f1845
JaySan169 <sanjay01061990@gmail.com>
2023-04-07 15:01:01 +0530

    1st

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -100,7 +100,6 @@ echo -n "Starting $COMPONENT service"
 systemctl daemon-reload
 systemctl start $COMPONENT
 systemctl enable $COMPONENT &>> $LOGFILE
-echo -n -e "\e[32m___________ $COMPONENT installation completed______________\e[0m"
 stat $?
 } 
 
