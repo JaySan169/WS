aa44112c47684986bf931bbc987cb364c852f7df
JaySan169 <sanjay01061990@gmail.com>
2023-04-10 17:56:43 +0530

    1st

--- robot/create_server.sh
+++ robot/create_server.sh
@@ -17,7 +17,7 @@ echo $COMPONENT
 
 create_server()
 {
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID Tags=[{Key=Name,Value=${COMPONENT}}] | jq &>> $LOGFILE
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq &>> $LOGFILE
 }
 
 if [ "$1" == "all" ] ; then
