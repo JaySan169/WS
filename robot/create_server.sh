f3cc4c9aeb1bfbb32d45a3d393852e40445e6193
JaySan169 <sanjay01061990@gmail.com>
2023-04-10 17:34:19 +0530

    1st

--- robot/create_server.sh
+++ robot/create_server.sh
@@ -7,5 +7,23 @@ fi
 
 COMPONENT=$1
 
-ZONE_ID=""
+AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
+SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=sg3 | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 
+echo "AMI ID Used to launch instance is : $AMI_ID"
+echo "SG ID Used to launch instance is : $SGID"
+echo $COMPONENT
+
+create_server()
+{
+aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID
+}
+
+if [ "$1" == "all" ] ; then
+    for comp in frontend catalogue cart user shipping payment mongodb mysql rabbitmq redis; do 
+        COMPONENT=$comp
+        createServer
+    done 
+else 
+        createServer 
+fi 
