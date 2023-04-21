0089fe9a0de3c53bb1b57df7a45b10c8c78c077f
JaySan169 <sanjay01061990@gmail.com>
2023-04-21 15:12:07 +0530

    1st

--- robot/create_server.sh
+++ robot/create_server.sh
@@ -7,17 +7,17 @@ fi
 
 COMPONENT=$1
 ZONE_ID="Z044318916WL2NSIWQC4A"
-AMI_ID="ami-0c1d144c8fdd8d690"
-SGID="sg-09c4f9587ca8ec0f1"
+#AMI_ID="ami-0c1d144c8fdd8d690"
+#SGID="sg-09c4f9587ca8ec0f1"
 
-#AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
-#SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=sg3 | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
+AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
+SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=sg3 | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 echo "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
 
 createServer() {
-    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
     sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json 
 
