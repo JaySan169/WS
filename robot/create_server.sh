27d7bf8163469e9c8402243ac1404d068831a5e1
JaySan169 <sanjay01061990@gmail.com>
2023-06-26 17:09:37 +0530

    1st

--- robot/create_server.sh
+++ robot/create_server.sh
@@ -15,12 +15,14 @@ AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=sg3 | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 echo "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SGID"
-echo "$COMPONENT-${ENV}"
+echo "${COMPONENT}-${ENV}"
 
 createServer() {
-    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${$COMPONENT-${ENV}}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+    [].PrivateIpAddress' | sed -e 's/"//g')
 
-    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT-${ENV}/" route53.json > /tmp/dns.json 
+    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}-${ENV}/" route53.json > /tmp/dns.json 
 
     echo -n "Creating the DNS Record ********"
     aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/dns.json | jq 
