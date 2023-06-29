3ddbe6a61df7938771e18ed5b452479566701ad3
JaySan169 <sanjay01061990@gmail.com>
2023-06-29 18:37:01 +0530

    1st

--- robot/install_maven.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget http://www.eu.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
-tar xzf apache-maven-3.5.0-bin.tar.gz
-mkdir /opt/maven
-mv apache-maven-3.5.0/ /opt/maven/
-alternatives --install /usr/bin/mvn mvn /opt/maven/apache-maven-3.5.0/bin/mvn 1
-alternatives --config mvn
