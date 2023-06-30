84b1a6b73faae3acbdf9987811e52b3db68cc5ca
JaySan169 <sanjay01061990@gmail.com>
2023-07-01 00:07:39 +0530

    1st

--- robot/install_jenkins.sh
+++ robot/install_jenkins.sh
@@ -11,23 +11,10 @@ Stat() {
     fi 
 }
 
-yum install fontconfig java-11-openjdk-devel wget -y
-echo -e "\e[32m java-11-openjdk-devel......INSTALLATION SUCCESSFUL\e[0m"
-
-sudo curl –sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-
-sudo yum install –y nodejs
-echo -e "\e[32m nodejs.....INSTALLATION SUCCESSFUL\e[0m"
-
-npm i jslint
-echo -e "\e[32m jslint.....INSTALLATION SUCCESSFUL\e[0m"
-
-sudo bash install_sonar_scanner.sh
-echo -e "\e[32m sonar_scanner.....INSTALLATION SUCCESSFUL\e[0m"
+yum install fontconfig java-11-openjdk-devel wget -y  &>/tmp/jinstall.log
 Stat $?
 
 pip install boto &>/tmp/jinstall.log
-echo -e "\e[32m boto.....INSTALLATION SUCCESSFUL\e[0m"
 Stat $?
 
 wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo &>>/tmp/jinstall.log 
